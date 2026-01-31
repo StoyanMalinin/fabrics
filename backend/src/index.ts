71fb71b07edd354bd439f66169f6655d5b1c8ed0
Stoyan Malinin <stoyanmalinin2002@gmail.com>
2026-01-31 19:17:07 +0100

    first successful clerk api call

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
+import 'dotenv/config' // Load environment variables from .env file
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
+app.use('/api/*', cors())
 app.use('*', clerkMiddleware())
 
 app.get('/', (c) => {
--- mobile/app/(tabs)/main-page.tsx
+++ mobile/app/(tabs)/main-page.tsx
@@ -1,16 +1,42 @@
-import { useUser } from "@clerk/clerk-expo";
+import { useAuth, useUser } from "@clerk/clerk-expo";
 import { Text } from "react-native";
 import { SignOutButton } from '@/components/sign-out-button'
+import { useEffect, useState } from "react";
 
 export function MainPage() {
     const { user } = useUser();
+    const { getToken } = useAuth();
+    const [userId, setUserId] = useState<string | null>(null);
+
+    useEffect(() => {
+        const f = async () => {
+            const token = await getToken();
+            const response = await fetch('http://192.168.1.91:3000/api/auth-test', {
+                headers: {
+                Authorization: `Bearer ${token}`
+                }
+            });
+
+            console.log("Response status:", response.status);
+
+            const data = await response.json();
+            setUserId(data.userId);
+        }
+
+        f();
+    }, []);
     
     if (!user) {
-      throw new Error("User is not signed in - this should be unreachable");
+        throw new Error("User is not signed in - this should be unreachable");
+    }
+
+    if (userId === null) {
+        return <Text>Loading...</Text>
     }
 
     return <>
-      <Text>Hello {user.emailAddresses[0].emailAddress}</Text>
-      <SignOutButton />
+        <Text>Hello {user.emailAddresses[0].emailAddress}</Text>
+        <Text>Your user ID is: {userId}</Text>
+        <SignOutButton />
     </>
 }
