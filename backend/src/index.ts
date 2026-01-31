5f48e9b9703f1c70ebe7ed8773a8d6d6e389664d
Stoyan Malinin <stoyanmalinin2002@gmail.com>
2026-01-31 18:39:36 +0100

    add clerk to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,29 @@
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
+app.use('*', clerkMiddleware())
+
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+    const res = getAuth(c);
+    return c.text('Hello Hono!')
+})
+
+app.get('/api/auth-test', (c) => {
+    const { isAuthenticated, userId } = getAuth(c)
+
+    if (!isAuthenticated) {
+        return c.json({ authenticated: false })
+    }
+
+    return c.json({authenticated: true, userId: userId})
 })
 
 serve({
-  fetch: app.fetch,
-  port: 3000
+    fetch: app.fetch,
+    port: 3000
 }, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
+    console.log(`Server is running on http://localhost:${info.port}`)
 })
